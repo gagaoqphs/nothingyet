08dc2638578420a3435013e6ed6b4f4a7e0070ff
soulmate-dot <83920811+soulmate-dot@users.noreply.github.com>
2021-05-11 18:01:09 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -27,7 +27,7 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""➼ Helloowww 👋 {message.from_user.first_name}! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n➼ Do you want me to play music in your Telegram groups'voice chats? Please click the " cσммαη∂s " button below to know how you can use me.\n\n➼ Use the buttons below to know more about me ❤️🔥\n\n➼ Contact my owner [🔥𝕐𝖔𝖚𝖗 - 𝕯𝖆𝖉𝖉𝕪🔥](https://t.me/FallenAngel_xD)\n\nA project by @FallenAngel_xD""",
+        f"""➼ Helloowww 👋 {message.from_user.first_name}! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n➼ Do you want me to play music in your Telegram groups'voice chats? Please click the " cσммαη∂s " button below to know how you can use me.\n\n➼ Use the buttons below to know more about me ❤️🔥\n\n➼ Contact my owner [🔥『 𝐒𝐎𝐔𝐋𝐌𝐀𝐓𝐄 』🔥](https://t.me/unknown_soulmate)\n\nA project by @unknown_soulmate""",
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
@@ -35,11 +35,11 @@ async def start(_, message: Message):
                         "📜 cσммαη∂s 📜", url="https://telegra.ph/%F0%9D%95%90%F0%9D%96%94%F0%9D%96%9A%F0%9D%96%97---%F0%9D%95%AF%F0%9D%96%86%F0%9D%96%89%F0%9D%96%89%F0%9D%95%AA-%EA%97%84-04-26")
                   ],[
                     InlineKeyboardButton(
-                        "❤️ αвσυт σху ❤️", url="https://t.me/aboutoxy"
+                        "❤️ αвσυт 『 𝐒𝐎𝐔𝐋𝐌𝐀𝐓𝐄 』 ❤️", url="https://t.me/unknownn_soulmate"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "🔥 σғғιcιαℓ gяσυρ 🔥", url="https://t.me/X_F0RCE_TEAM"
+                        "🔥 σғғιcιαℓ gяσυρ 🔥", url="https://t.me/joinchat/MK4RoLY0icRjZGQ1"
                     )]
             ]
         ),
@@ -53,7 +53,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🔥 мү σωηεя 🔥", url="https://t.me/FallenAngel_xD")
+                        "🔥 мү σωηεя 🔥", url="https://t.me/unknown_soulmate")
                 ]
             ]
         )
